bc999846a7948156429cfdf8ce941b454916e6ec
sabbir1054 <mdsabbir1054@gmail.com>
2023-05-30 07:52:55 +0600

    prettier fix updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,8 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.join(process.cwd(), ".env") });
-
+import dotenv from 'dotenv'
+import path from 'path'
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
   port: process.env.PORT,
   databaseUrl: process.env.DATABASE_URL,
-};
+}
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function mainBootstrap() {
   try {
-    await mongoose.connect(config.databaseUrl as string);
+    await mongoose.connect(config.databaseUrl as string)
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
-    console.log("Database is connected");
+      console.log(`Example app listening on port ${config.port}`)
+    })
+    console.log('Database is connected')
   } catch (err) {
-    console.log("Failed to connect Database", err);
+    console.log('Failed to connect Database', err)
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-mainBootstrap();
+mainBootstrap()
