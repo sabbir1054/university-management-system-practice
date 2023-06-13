c43b9f24f23e2f795ca7ea91cdaaad5f2e5a9920
sabbir1054 <mdsabbir1054@gmail.com>
2023-06-13 18:44:52 +0600

    Winston Basic Setup [Done]

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import logger from './shared/logger'
 
 async function mainBootstrap() {
   try {
     await mongoose.connect(config.databaseUrl as string)
-    // eslint-disable-next-line no-console
-    console.log(`🛢   Database is connected successfully`)
+
+    logger.info(`🛢   Database is connected successfully`)
 
     app.listen(config.port, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Application  listening on port ${config.port}`)
+      logger.info(`Application  listening on port ${config.port}`)
     })
   } catch (err) {
-    // eslint-disable-next-line no-console
-    console.log('Failed to connect database', err)
+    logger.error('Failed to connect database', err)
   }
 }
 
