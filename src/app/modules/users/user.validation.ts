270ec558c83744f89a0d8e5a4c9b3677b11fd6e3
sabbir1054 <mdsabbir1054@gmail.com>
2023-06-14 21:06:15 +0600

    [Done] Make Zod Error Handler for Schema

---
+++ src/app/modules/users/user.validation.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+// zod validation
+const createUserZodSchema = z.object({
+  body: z.object({
+    role: z.string({ required_error: 'role is required' }),
+    password: z.string().optional(),
+  }),
+});
+// await createUserZodSchema.parseAsync(req);
+
+export const UserValidation = {
+  createUserZodSchema,
+};
