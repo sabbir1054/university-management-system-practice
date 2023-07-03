af3bd7b3356f544d0b7a434cc25b50d2c24e2084
sabbir1054 <mdsabbir1054@gmail.com>
2023-07-03 11:41:28 +0600

    [Done] impliment change password

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validedRequest';
 import { AuthController } from './auth.controller';
 import { AuthValidation } from './auth.validation';
@@ -15,5 +17,14 @@ router.post(
   validateRequest(AuthValidation.refreshTokenZodSchema),
   AuthController.refreshToken
 );
-
+router.post(
+  '/change-password',
+  auth(
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT,
+    ENUM_USER_ROLE.SUPER_ADMIN
+  ),
+  AuthController.changePassword
+);
 export const AuthRoutes = router;
